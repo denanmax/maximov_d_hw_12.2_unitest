bd7cbcc39df35432c32c24727606f88888d2c678
denanmax <deshis93@gmail.com>
2023-04-27 16:06:12 +0200

    100% coverage of tests completed

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,13 +7,23 @@ class TestArrs(unittest.TestCase):
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3], 0, None), 1)
+        self.assertEqual(arrs.get([1, 2, 3], 10, "test"), "test")
+
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([]), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -7), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -3), [2, 3, 4])
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+
+
+
+
+
 
 
